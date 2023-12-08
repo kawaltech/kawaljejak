d64323178f6dc4137940026816c76d4f3f92be96
Zain Fathoni <me@zainf.dev>
2023-12-09 01:19:08 +0800

    build: Add new dependencies and tailwind preset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("@kawalcovid19/tailwind-preset-wbw")],
   theme: {
     extend: {},
   },
