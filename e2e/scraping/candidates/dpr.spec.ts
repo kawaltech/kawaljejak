b4a5be501bba25e3d6f0a1bf2e564df3a8c2f16b
Zain Fathoni <me@zainf.dev>
2023-12-09 23:26:49 +0800

    fix: update import path for dapil file

--- e2e/scraping/candidates/dpr.spec.ts
+++ e2e/scraping/candidates/dpr.spec.ts
@@ -1,18 +1,16 @@
 import { test } from "@playwright/test";
 import type { CandidateDetails } from "e2e/models/candidates";
 import { createCandidateDetailsExtractor } from "e2e/utils/extractors";
-import ACEH_II from "../../fixtures/dpr/1102_ACEH II.json" assert { type: "json" };
+import dapil from "../../fixtures/dpr/1201_SUMATERA UTARA I.json" assert { type: "json" };
 
 test.describe.configure({ mode: "parallel" });
 
 // TODO: import dapil files dynamically from fixtures/dpr/*.json
-const { id, name, candidates } = ACEH_II;
-
-candidates.forEach((candidate: CandidateDetails, index: number) => {
+dapil.candidates.forEach((candidate: CandidateDetails, index: number) => {
   test(
     `fetch DPR dapil ${"ACEH II"} candidate details for ${candidate.name}`,
     createCandidateDetailsExtractor({
-      dapil: { id, name },
+      dapil,
       directory: "dpr",
       url: "https://infopemilu.kpu.go.id/Pemilu/Dct_dpr",
       index,
