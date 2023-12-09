ac11bf259ef6e29258cc9a55f2917f350f3f16d1
Zain Fathoni <me@zainf.dev>
2023-12-09 16:35:19 +0800

    refactor(e2e): refactor scraping/dpr.spec.ts

--- e2e/scraping/dpr.spec.ts
+++ e2e/scraping/dpr.spec.ts
@@ -17,10 +17,39 @@ test("has title", async ({ page }) => {
 
   const dapil = dapils.dpr[0];
 
-  console.debug(dapils);
   await page.getByRole("option", { name: dapil.name, exact: true }).click();
 
   await page
     .getByRole("cell", { name: "Harap Pilih Dapil Terlebih" })
     .waitFor({ state: "hidden", timeout: 60_000 });
+
+  const rows = page.locator("tr");
+  // TODO: traverse all rows
+
+  const headers = rows.first().locator("td");
+  const headerTexts = [];
+  for (let i = 0; i < (await headers.count()); i++) {
+    const textContent = await headers.nth(i).textContent();
+    headerTexts.push(textContent);
+    console.log(i, textContent);
+  }
+
+  const fields = rows.nth(1).locator("td");
+
+  const textContents = [];
+  for (let i = 0; i < (await fields.count()); i++) {
+    const textContent = await fields.nth(i).textContent();
+    textContents.push(textContent);
+    console.log(i, textContent);
+  }
+
+  const party = await fields.nth(0).textContent();
+  const number = parseInt(
+    (await fields.nth(2).textContent())?.substring(10) ?? "0",
+  );
+  const name = await fields.nth(4).textContent();
+  const gender = await fields.nth(5).textContent();
+  const address = await fields.nth(6).textContent();
+
+  console.debug({ party, number, name, gender, address });
 });
