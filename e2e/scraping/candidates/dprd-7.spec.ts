e8f2773f9cfd9c677e538f5f5ffda21dee98f2a4
Zain Fathoni <me@zainf.dev>
2023-12-11 05:04:37 +0800

    fix(e2e): Update slicing indices for dprd-5, dprd-6, and dprd-7 specs

--- e2e/scraping/candidates/dprd-7.spec.ts
+++ e2e/scraping/candidates/dprd-7.spec.ts
@@ -12,7 +12,7 @@ test.describe.configure({ mode: "parallel" });
 const directory = DIRECTORIES.DPRD_2;
 const url = URLS.DPRD_2;
 
-dprd2.slice(1500, 1800).forEach(({ id, name }) => {
+dprd2.slice(1622, 1800).forEach(({ id, name }) => {
   const dapil = readJSON<DapilWithCandidates>(
     getDapilFilename({ directory, dapil: { id, name } }),
   );
